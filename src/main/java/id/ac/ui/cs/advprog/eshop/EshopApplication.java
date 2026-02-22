518e8c7f25910439999cea69ae39292efa095c91
dinovaslate <dinovasala3@gmail.com>
2026-02-22 18:24:09 +0700

    Fix PMD warning in EshopApplication

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@SuppressWarnings("PMD.UseUtilityClass")
 public class EshopApplication {
 
     public static void main(String[] args) {
+        new EshopApplication().run(args);
+    }
+
+    void run(String[] args) {
         SpringApplication.run(EshopApplication.class, args);
     }
 
