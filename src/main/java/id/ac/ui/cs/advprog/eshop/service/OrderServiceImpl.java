247184bf50b3e122adbc3834bebf72ab13f285ec
dinovaslate <dinovasala3@gmail.com>
2026-03-09 22:20:20 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -14,21 +15,31 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order createOrder(Order order) {
-        return null;
+        Order existingOrder = orderRepository.findById(order.getId());
+        if (existingOrder != null) {
+            return existingOrder;
+        }
+        return orderRepository.save(order);
     }
 
     @Override
     public Order updateStatus(String orderId, String status) {
-        return null;
+        Order order = findById(orderId);
+        if (order == null) {
+            throw new NoSuchElementException("Order not found");
+        }
+
+        order.setStatus(status);
+        return orderRepository.save(order);
     }
 
     @Override
     public Order findById(String orderId) {
-        return null;
+        return orderRepository.findById(orderId);
     }
 
     @Override
     public List<Order> findAllByAuthor(String author) {
-        return List.of();
+        return orderRepository.findAllByAuthor(author);
     }
 }
